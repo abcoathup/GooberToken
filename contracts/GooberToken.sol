8a282c6be3e383931b95b9372d8e59baedc3beea
Andrew B Coathup <28278242+abcoathup@users.noreply.github.com>
2018-05-02 18:25:03 +1000

    Updated test

--- contracts/GooberToken.sol
+++ contracts/GooberToken.sol
@@ -1,14 +1,17 @@
-pragma solidity ^0.4.21;
+pragma solidity 0.4.21;
 
 import "zeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+
 
 /**
  * @title GooberToken
  * @dev ERC20 Token
  */
 contract GooberToken is CappedToken, DetailedERC20 {
-  function GooberToken() 
-    CappedToken(10**9*10**18) 
-    DetailedERC20("Goober Token", "GOOB", 18) public {}
+    function GooberToken() 
+        public
+        CappedToken(10**9*10**18) 
+        DetailedERC20("Goober Token", "GOOB", 18) {
+        }
 }
--- test/GooberToken.test.js
+++ test/GooberToken.test.js
@@ -1,40 +1,92 @@
 const GooberToken = artifacts.require('GooberToken');
 
-contract('GooberToken', accounts => {
+contract('GooberToken', function ([owner, anotherAccount]) {
   let token;
-  const creator = accounts[0];
+
+  const _name = 'Goober Token';
+  const _symbol = 'GOOB';
+  const _decimals = 18;
+  const _cap = (10 ** 9) * (10 ** _decimals);
 
   beforeEach(async function () {
-    token = await GooberToken.new({ from: creator });
+    token = await GooberToken.new({ from: owner });
   });
 
-  it('has a name', async function () {
-    const name = await token.name();
-    assert.equal(name, 'Goober Token');
+  describe('token details', function () {
+    it('has a name', async function () {
+      const name = await token.name();
+      assert.equal(name, _name);
+    });
+
+    it('has a symbol', async function () {
+      const symbol = await token.symbol();
+      assert.equal(symbol, _symbol);
+    });
+
+    it('has 18 decimals', async function () {
+      const decimals = await token.decimals();
+      assert(decimals.eq(_decimals));
+    });
+
+    it('has a 0 total supply', async function () {
+      const totalSupply = await token.totalSupply();
+      assert(totalSupply.eq(0));
+    });
+  
+    it('has a 0 owner supply', async function () {
+      const ownerBalance = await token.balanceOf(owner);
+      assert(ownerBalance.eq(0));
+    });
+    
+    it('has the cap set', async function () {
+      const tokenCap = await token.cap();
+      assert(tokenCap.eq(_cap));
+    });
   });
 
-  it('has a symbol', async function () {
-    const symbol = await token.symbol();
-    assert.equal(symbol, 'GOOB');
-  });
+  describe('token minting', function () {
+    const amount = 100;
+    const from = owner;
 
-  it('has 18 decimals', async function () {
-    const decimals = await token.decimals();
-    assert(decimals.eq(18));
-  });
+    it('can mint to owner', async function () {
+      await token.mint(owner, amount, { from });
 
-  it('assigns the initial total supply to the creator', async function () {
-    const totalSupply = await token.totalSupply();
-    const creatorBalance = await token.balanceOf(creator);
+      const balance = await token.balanceOf(owner);
+      assert.equal(balance, amount);
+    });
 
-    assert(creatorBalance.eq(totalSupply));
+    it('can mint to another account', async function () {
+      await token.mint(anotherAccount, amount, { from });
 
-    const receipt = web3.eth.getTransactionReceipt(token.transactionHash);
-    // const logs = decodeLogs(receipt.logs, GooberToken, token.address);
-    // assert.equal(logs.length, 1);
-    // assert.equal(logs[0].event, 'Transfer');
-    // assert.equal(logs[0].args.from.valueOf(), 0x0);
-    // assert.equal(logs[0].args.to.valueOf(), creator);
-    // assert(logs[0].args.value.eq(totalSupply));
+      const balance = await token.balanceOf(anotherAccount);
+      assert.equal(balance, amount);
+    });
+
+    it('cannot mint after minting stopped', async function () {
+      await token.finishMinting({ from});
+
+      try {
+        await token.mint(owner, amount, { from });
+
+        assert.fail('Expected revert not received');
+      } catch (error) {
+        const revertFound = error.message.search('revert') >= 0;
+        assert(revertFound, `Expected "revert", got ${error} instead`);
+      }
+    });
+
+    it('cannot mint above cap', async function () {
+      await token.mint(owner, amount, { from });
+
+      try {
+        await token.mint(owner, _cap, { from });
+
+        assert.fail('Expected revert not received');
+      } catch (error) {
+        const revertFound = error.message.search('revert') >= 0;
+        assert(revertFound, `Expected "revert", got ${error} instead`);
+      }
+    });
+
   });
 });
