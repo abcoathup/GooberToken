378ee8496f4bec5f8e54e0e0b605079aad94b154
Andrew B Coathup <28278242+abcoathup@users.noreply.github.com>
2018-06-06 20:35:55 +1000

    Update contructor

--- contracts/GooberToken.sol
+++ contracts/GooberToken.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.4.24;
 
+
 import "openzeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
@@ -9,10 +10,6 @@ import "openzeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
  * @title GooberToken
  * @dev ERC20 Token
  */
-contract GooberToken is CappedToken, DetailedERC20, ERC827Token {
-    constructor() public 
-
-    CappedToken(10**9*10**18) 
-    DetailedERC20("Goober Token", "GOOB", 18) {
-    }
+contract GooberToken is CappedToken(10**9*10**18), 
+    DetailedERC20("Goober Token", "GOOB", 18), ERC827Token {
 }
